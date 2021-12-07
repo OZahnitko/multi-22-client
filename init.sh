517a2ba66d239c0410e81992340cc38555f6b046
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-07 16:33:44 +1000

    one moooooore time

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull ozahnitko/multi-22-client:latest
-docker container stop client_app
-docker container rm client_app
+docker container stop client_app || :
+docker container rm client_app || :
 docker run -d -p 80:80 --name client_app ozahnitko/multi-22-client:latest
